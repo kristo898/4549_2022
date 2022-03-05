ff5e237dab1ba34d53187ae580418e5991219d09
kristo898 <60372992+kristo898@users.noreply.github.com>
2022-03-05 15:03:51 -0600

    Update DriveSubsystem.java

--- 4549-Command-2022/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ 4549-Command-2022/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,43 +4,61 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+//import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. 
    * @return */
-  final Spark driveLeftMotor = new Spark(0);
-  final Spark driveRightMotor = new Spark(1);
-  final Spark driveBackRightMotor = new Spark(2);
-  final Spark driveBackLeftMotor = new Spark(3);
-  final Encoder rightEncoder = new Encoder(0, 1);
+  final CANSparkMax driveLeftMotor = new CANSparkMax(3, MotorType.kBrushless);
+  final CANSparkMax driveRightMotor = new CANSparkMax(11, MotorType.kBrushless);
+  final CANSparkMax driveBackRightMotor = new CANSparkMax(2, MotorType.kBrushless);
+  final CANSparkMax driveBackLeftMotor = new CANSparkMax(6, MotorType.kBrushless);
+  final MotorControllerGroup leftMotorGroup = new MotorControllerGroup(driveLeftMotor, driveBackLeftMotor);
+  final MotorControllerGroup rightMotorGroup = new MotorControllerGroup(driveRightMotor, driveBackRightMotor);
+  final DifferentialDrive drive = new DifferentialDrive(leftMotorGroup, rightMotorGroup);
+  /*final Encoder rightEncoder = new Encoder(0, 1);
   final Encoder leftEncoder = new Encoder(2, 3);
+  final Encoder backrightEncoder = new Encoder(4, 5);
+  final Encoder backleftEncoder = new Encoder(6, 7);
   final double kEncoderTick2Meter = 1.0 / 4096.0 * 0.128 * Math.PI;
 
-  double getEncoderMeters() {
+  public double getEncoderMeters() {
     return (leftEncoder.get() + -rightEncoder.get()) / 2 * kEncoderTick2Meter;
   }
+  public double getbackEncoderMeters() {
+    return (backleftEncoder.get() + -backrightEncoder.get()) / 2 * kEncoderTick2Meter;
+  }*/
   public DriveSubsystem() {
    driveRightMotor.setInverted(false);
    driveLeftMotor.setInverted(true);
-   driveBackLeftMotor.setInverted(true);
-   driveBackRightMotor.setInverted(false);
-   leftEncoder.setReverseDirection(false);
+   driveBackRightMotor.follow(driveRightMotor);
+   driveBackLeftMotor.follow(driveLeftMotor);
+   driveLeftMotor.restoreFactoryDefaults();
+   driveBackLeftMotor.restoreFactoryDefaults();
+   driveRightMotor.restoreFactoryDefaults();
+   driveBackRightMotor.restoreFactoryDefaults();
+   /*leftEncoder.setReverseDirection(false);
    rightEncoder.setReverseDirection(true);
+   backleftEncoder.setReverseDirection(false);
+   backrightEncoder.setReverseDirection(true);*/
   }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    SmartDashboard.putNumber("Drive encoder value", getEncoderMeters());
-  }
+    /*SmartDashboard.putNumber("Drive encoder value", getEncoderMeters());
+    SmartDashboard.putNumber("Drive back encoder value", getbackEncoderMeters());*/
+    }
+    
+   
     public void setMotors(double leftSpeed, double rightSpeed){
-      driveLeftMotor.set(leftSpeed);
-      driveRightMotor.set(rightSpeed);
-      driveBackRightMotor.set(leftSpeed);
-      driveBackLeftMotor.set(rightSpeed);
+      drive.arcadeDrive(leftSpeed, rightSpeed);
     }
   @Override
   public void simulationPeriodic() {
